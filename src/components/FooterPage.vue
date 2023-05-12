f3c3b5f744e71a0dd6a2a4aa531c2b49ce8542d8
YannPernette <yann.pernette@edu.univ-fcomte.fr>
2023-05-12 11:30:43 +0200

    commencement footer html

--- src/components/FooterPage.vue
+++ src/components/FooterPage.vue
@@ -1,1 +1,35 @@
-<template>Footer du site</template>
+<template>
+
+<footer>
+
+    <nav>
+
+        <div>
+            <h4>Mentions Légales</h4>
+            <ul>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+            </ul>
+        </div>
+
+        <div>
+            <h4>Contact</h4>
+            <ul>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+            </ul>
+        </div>
+
+        <div>
+            <h4>Abonnement</h4>
+            <ul>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+            </ul>
+        </div>
+
+    </nav>
+
+</footer>
+
+</template>
