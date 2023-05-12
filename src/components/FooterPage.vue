aaf7592e35796429f937c6328274f8bb5e4de2f2
YannPernette <yann.pernette@edu.univ-fcomte.fr>
2023-05-12 11:48:45 +0200

    footer semantique fini

--- src/components/FooterPage.vue
+++ src/components/FooterPage.vue
@@ -7,28 +7,45 @@
         <div>
             <h4>Mentions Légales</h4>
             <ul>
-                <li><a href="#"></a></li>
-                <li><a href="#"></a></li>
+                <li><a href="#">Politique des cookies</a></li>
+                <li><a href="#">Politique de confidentialité</a></li>
             </ul>
         </div>
+
+        <span></span>
 
         <div>
             <h4>Contact</h4>
             <ul>
-                <li><a href="#"></a></li>
-                <li><a href="#"></a></li>
+                <li><a href="#">Contactez-nous</a></li>
+                <li><a href="#">Qui sommes-nous ?</a></li>
             </ul>
         </div>
+
+        <span></span>
 
         <div>
             <h4>Abonnement</h4>
             <ul>
-                <li><a href="#"></a></li>
-                <li><a href="#"></a></li>
+                <li><a href="#">Voir l'offre</a></li>
+                <li><a href="#">Souscrire</a></li>
             </ul>
         </div>
 
     </nav>
+
+    <div>
+
+        <p>Suivez-nous</p>
+
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+
+    </div>
 
 </footer>
 
