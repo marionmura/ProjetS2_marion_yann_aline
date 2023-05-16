d0ba7e39ebe73d981de9092e132362963996006d
YannPernette <yann.pernette@edu.univ-fcomte.fr>
2023-05-16 08:31:23 +0200

    html semantique a propos site

--- src/pages/aPropos.vue
+++ src/pages/aPropos.vue
@@ -1,5 +1,13 @@
 <template>
-    <h1>Page à Propos</h1>
 
-    <RouterLink to="/">Home</RouterLink>
-  </template>
+  <h1>À Propos du site</h1>
+
+  <div>
+    <img src="../img/departement_MMI_montbeliard.webp" alt="Photographie du département MMI Montbéliard">
+    <p>Ce site web est un projet permettant d’informer et d’aider les gens qui découvrent les IA et qui souhaiteraient les utiliser. Il y a des tutos pour débutants disponibles et il est possible de mettre en favoris vos articles préférés pour les retrouver rapidement.</p>
+    <p>Projet réalisé dans le cadre d’un exercice pour le département MMI à Montbéliard.</p>
+  </div>
+
+  <h1>Et de l'équipe...</h1>
+
+</template>
