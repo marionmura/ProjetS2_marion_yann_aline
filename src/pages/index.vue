434a39d1ddc345500bb614c0fad9d606e8d5442c
Rose-Aline <116867946+Rose-Aline@users.noreply.github.com>
2023-05-16 09:17:12 +0200

    Page article html

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,4 +2,5 @@
     <h1>Page d'accueil</h1>
     <RouterLink to="/aPropos">À Propos</RouterLink>
     <RouterLink to="/Connexion">Connexion</RouterLink>
+    <RouterLink to="/Article">Article</RouterLink>
   </template>
