7801b9c81cc2a3c664f09b60a3435bc1a7d58d30
steak-osaure <marion.mura090@gmail.com>
2023-05-16 10:30:54 +0200

    page connexion en tailwind ok

--- src/pages/Connexion.vue
+++ src/pages/Connexion.vue
@@ -27,23 +27,29 @@ export default {
 
 <template  >
   <div class="bg-grisbg text-white">
-    <h1 class="ml-4 mr-4 p-4 text-center text-violet border-2 rounded-lg">Connexion</h1>
+    <h1 class="ml-[2rem] mr-[2rem] p-4 text-center text-violet border-2 rounded-lg">Connexion</h1>
     <div class="flex flex-col">
         <p>navigation temporaire</p>
         <RouterLink class="bg-violet text-noirbackground font-bold" to="/Home">Home</RouterLink>
         <RouterLink class="bg-violet text-noirbackground font-bold" to="/aPropos">A Propos</RouterLink>
     </div>
     
-    <div class="mt-5 text-white">
-      <label class="text-violet" width="50px">Email </label><input type="email" id="login" placeholder="écrire ici" class="bg-grisbg border-2 rounded-md text-violet p-2 m-1 focus:outline-none focus:ring-2 focus:ring-rouge focus:border-0">
+    <div class="mt-5  pb-10 flex flex-col mr-[2rem] ml-[2rem] text-white">
+      <label class="text-violet" width="50px">Email (*)</label>
       <p></p>
-      <label class="text-violet" width="50px">Mot de passe  </label><input  type="password" id="passwd" placeholder="écrire ici" class="bg-grisbg border-2 rounded-md text-violet p-2 m-1 focus:outline-none focus:ring-2 focus:ring-rouge focus:border-0" >
+      <input type="email" id="login" placeholder="écrire ici" class="bg-grisbg border-2 rounded-md text-violet p-2 m-1 ml-0 focus:outline-none focus:ring-2 focus:ring-rouge focus:border-0">
+      <p class="mb-5"></p>
+      <label class="text-violet" width="50px">Mot de passe (*) </label><p></p>
+      <input  type="password" id="passwd" placeholder="écrire ici" class="bg-grisbg border-2 rounded-md text-violet p-2 ml-0 m-1 focus:outline-none focus:ring-2 focus:ring-rouge focus:border-0" >
       <p></p>
       
-      
-      <button class="p-1 m-1 pl-3 pr-3 rounded-full bg-grisfooter text-white" v-on:click="register()">s'inscrire</button>
-      <button class="p-1 pl-3 pr-3  m-1 text-noirbackground font-bold rounded-full bg-white" v-on:click="login()">connexion</button>
       <p class="text-violet " v-on:click="reset()">J'ai oublié mon mot de passe</p>
+
+      <div class="flex flex-row justify-end mt-5">
+        <button class="p-1  m-1 pl-3 pr-3 rounded-full bg-grisfooter text-white" v-on:click="register()">s'inscrire</button>
+        <button class="p-1 pl-3 pr-3  m-1 text-noirbackground font-bold rounded-full bg-white" v-on:click="login()">connexion</button>
+        
+      </div>
     </div>
 
     </div>
