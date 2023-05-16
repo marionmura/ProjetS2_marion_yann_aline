292c5b082911f876db57bfaf173d949eb4cf3c08
steak-osaure <marion.mura090@gmail.com>
2023-05-16 16:07:36 +0200

    style menu ok

--- src/components/HeaderPage.vue
+++ src/components/HeaderPage.vue
@@ -9,11 +9,9 @@ import {RouterLink} from 'vue-router'
 
 <template > 
     <header class=" bg-noirbackground text-violet h-20 fixed z-10 flex w-full translate-y-0 items-stretch justify-between">
-        <div class="">
+        <div class="flex flex-row justify-end">
             <RouterLink to="index"><IAmateurlogo/></RouterLink>
             <RouterLink to="/Connexion"><Usericon/></RouterLink>
-        </div>
-       
         
         <button class="relative z-10 h-4 w-6 flex flex-col justify-between lg:hidden"  
         @pointerdown="menuIsOpen = !menuIsOpen" >
@@ -21,16 +19,18 @@ import {RouterLink} from 'vue-router'
             <span class="block h-[2px] w-full bg-violet transition duration-300" :class="{'translate-y-[0px] -rotate-45':menuIsOpen}"></span>
             <span class="block h-[2px] w-full bg-violet transition duration-300" :class="{'translate-y-[-7px] -rotate-45':menuIsOpen}"></span>
         </button>
+
+        </div>
         <nav class="invisible opacity-0  lg:invisible fixed inset-0 h-screen w-screen bg-noirbackground"  :class="{'!visible !opacity-100':menuIsOpen}">
-            <ul class="mt-5 space-x-10 flex flex-col gap-4 ">
+            <ul class="mt-[80px] ml-[30px] space-x-10 flex flex-col gap-4 ">
                 <li><RouterLink class="active" to="/Espacepersonnel" >Espace personnel</RouterLink></li>
                 <li><RouterLink class="menu-link" to="/Mesfavoris">Mes favoris</RouterLink></li>
                 <li><RouterLink class="menu-link" to="/Mesarticlesenregistres">Mes articles enregistrés</RouterLink></li>
                 <li><RouterLink class="menu-link" to="/aPropos">À propos</RouterLink></li>
             </ul>
 
-            <span class="mt-5 mb-2 block h-[2px] w-[300px] bg-violet"></span>
-            <ul class=" mt-5 mb-5 space-x-10 flex flex-col gap-4 lg:flex-row">
+            <span class="mt-5 mb-2 ml-[2rem] block h-[2px] w-[300px] bg-violet"></span>
+            <ul class=" mt-[50px] ml-[30px] mb-5 space-x-10 flex flex-col gap-4 lg:flex-row">
                 <li><a class="menu-link" >IA tendances</a></li>
                 <li><a class="menu-link" href="">Articles récents</a></li>
                 <li><a class="mennu-link" href="">Tous les articles</a></li>
