1a8579262c8c4aae51f34e39cfc8601e8f313c7d
Rose-Aline <116867946+Rose-Aline@users.noreply.github.com>
2023-05-16 11:09:03 +0200

    debut css page article

--- src/pages/Article.vue
+++ src/pages/Article.vue
@@ -1,61 +1,72 @@
+<script setup lang="ts">
+import Like from '../components/icons/Like.vue';
+import Save from '../components/icons/Save.vue';
+import Ampouleicon from '../components/icons/Ampouleicon.vue';
+</script>
+
 <template>
-    <h1 class="bg-grisbg">Connexion</h1>
-    <div class="flex flex-col">
-        <RouterLink to="/Home">Home</RouterLink>
-        <RouterLink to="/aPropos">A Propos</RouterLink>
-        <h1>Midjourney</h1>
-        <p> Conçu par Open AI  </p>                
-        <li><Like /></li>
-        <li><Save /></li>
+    <body class=" bg-noirbackground text-white max-w-full">
+       
+        <h1 class="bg-grisbg">Connexion</h1>
+        <div class="flex flex-col">
+            <RouterLink to="/Home">Home</RouterLink>
+            <RouterLink to="/aPropos">A Propos</RouterLink>
+            <h1>Midjourney</h1>
+            <p> Conçu par Open AI  </p>                
+            <li><Like /></li>
+            <li><Save /></li>
+        </div>
 
-        <h3>Fonctionnalités : </h3>
-        <li>
-            IA générative
-            IA artistique
-            utilisation via discord
-        </li>
-        
-        <h2>Pourquoi utiliser Midjourney ?</h2>
-        <p>
-            Vous avez des difficultés en dessin ou juste pour représenter vos idées ? Alors la solution est simple ! Midjourney vous aidera, à partir d’une simple description de ce que vous souhaitez l’IA peut générer 4 images de styles différents et vous avez juste choisir votre préférée pour l’améliorer. 
-        </p>
+        <div class="pb-10 pt-20 bg-grisfooter rounded-3xl ;">
+            <h3>Fonctionnalités : </h3>
+            <ul>
+                <li>IA générative</li>
+                <li> IA artistique</li>
+                <li> utilisation via discord</li>
+            </ul>
 
-        <h2>Version Midjourney premium disponible</h2>
-        <p>
-            fonctionnalités bonus :
-            permet de générer de façon illimitée
-            l’offre gratuite est limitée à 25 lots d’images        
-        </p>
+            <h2>Pourquoi utiliser Midjourney ?</h2>
+            <p>
+                Vous avez des difficultés en dessin ou juste pour représenter vos idées ? Alors la solution est simple ! Midjourney vous aidera, à partir d’une simple description de ce que vous souhaitez l’IA peut générer 4 images de styles différents et vous avez juste choisir votre préférée pour l’améliorer. 
+            </p>
 
-        <icon></icon>
-        <h2>Comment utiliser Midjourney - tuto aide</h2>
-        <p>
-            Il faut au préalable avoir un compte discord
-            allez sur le site de Midjourney
-            défilez la page et cliquer sur “join our Discord”
-            le site vous redirigera vers un serveur discord où il faudra vérifier le capchat
+            <h2>Version Midjourney premium disponible</h2>
+            <p>
+                fonctionnalités bonus :
+                permet de générer de façon illimitée
+                l’offre gratuite est limitée à 25 lots d’images        
+            </p>
 
-            Pour utiliser l’IA
-            allez sur un salon nommé #newbies- (avec n’importe quel numéro)
-            entrez la description de l’image que vous souhaitez générer
-            attendre que l’IA génère l’image (1 minute environ)
+            <icon><Ampouleicon /></icon>
+            <h2>Comment utiliser Midjourney - tuto aide</h2>
+            <p>
+                Il faut au préalable avoir un compte discord
+                allez sur le site de Midjourney
+                défilez la page et cliquer sur “join our Discord”
+                le site vous redirigera vers un serveur discord où il faudra vérifier le capchat
 
-            possibilité d’améliorer une des 4versions proposées en cliquant sur une réaction en fin de message
-            possibilité d’améliorer l’image et de la retoucher
-            terminez en enregistrant l’image
+                Pour utiliser l’IA
+                allez sur un salon nommé #newbies- (avec n’importe quel numéro)
+                entrez la description de l’image que vous souhaitez générer
+                attendre que l’IA génère l’image (1 minute environ)
 
-            Vous pouvez répéter cette opération 25 fois uniquement, pour pouvoir en faire plus il faudra acheter un des abonnements proposés : 
-            un plan basique à 8 $/mois 
-            un plan standart à 24 $/mois (permet de générer sans limitations)
-            un plan pro à 48 $/mois (permet de générer sans limitations)
+                possibilité d’améliorer une des 4versions proposées en cliquant sur une réaction en fin de message
+                possibilité d’améliorer l’image et de la retoucher
+                terminez en enregistrant l’image
 
-            Les 3 abonnements vous permettront de créer des images directement dans vos messages privés et non plus sur le channel discord accessible aux milliers d’utilisateurs.
+                Vous pouvez répéter cette opération 25 fois uniquement, pour pouvoir en faire plus il faudra acheter un des abonnements proposés : 
+                un plan basique à 8 $/mois 
+                un plan standart à 24 $/mois (permet de générer sans limitations)
+                un plan pro à 48 $/mois (permet de générer sans limitations)
 
-            Retrouvez tout le mode d’emploi sur le site de Midjourney.
-        </p>
+                Les 3 abonnements vous permettront de créer des images directement dans vos messages privés et non plus sur le channel discord accessible aux milliers d’utilisateurs.
 
-        <h2>Articles qui pourraient vous intéresser</h2>
-        
-    </div>
-    
+                Retrouvez tout le mode d’emploi sur le site de Midjourney.
+            </p>
+        </div>
+        <div>
+            <h2>Articles qui pourraient vous intéresser</h2>
+            
+        </div>
+    </body>
 </template>
