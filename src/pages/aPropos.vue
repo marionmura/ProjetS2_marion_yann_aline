d6bbb1d3a4f7f7036a04779c4817da8ad9b150b4
YannPernette <yann.pernette@edu.univ-fcomte.fr>
2023-05-16 09:38:26 +0200

    a propos site tailwind

--- src/pages/aPropos.vue
+++ src/pages/aPropos.vue
@@ -1,13 +1,17 @@
 <template>
+<main class="bg-grisbg">
 
-  <h1>À Propos du site</h1>
+  <h1 class="lg:text-white">À Propos du site</h1>
 
-  <div>
-    <img src="../img/departement_MMI_montbeliard.webp" alt="Photographie du département MMI Montbéliard">
-    <p>Ce site web est un projet permettant d’informer et d’aider les gens qui découvrent les IA et qui souhaiteraient les utiliser. Il y a des tutos pour débutants disponibles et il est possible de mettre en favoris vos articles préférés pour les retrouver rapidement.</p>
-    <p>Projet réalisé dans le cadre d’un exercice pour le département MMI à Montbéliard.</p>
+  <div class="flex flex-col mx-5 my-10 p-3 bg-grisfooter border-solid border-2 border-violet rounded lg:flex-row lg:bg-transparent lg:mx-0 lg:p-0 lg:rounded-none lg:border-0 lg:border-t-2 lg:border-b-2">
+    <img class="rounded lg:rounded-none" src="../img/departement_MMI_montbeliard.webp" alt="Photographie du département MMI Montbéliard">
+    <div class="text-white text-base lg:text-xl lg:p-10">
+      <p class="my-3 lg:my-0 lg:mb-6">Ce site web est un projet permettant d’informer et d’aider les gens qui découvrent les IA et qui souhaiteraient les utiliser. Il y a des tutos pour débutants disponibles et il est possible de mettre en favoris vos articles préférés pour les retrouver rapidement.</p>
+      <p>Projet réalisé dans le cadre d’un exercice pour le département MMI à Montbéliard.</p>
+    </div>
   </div>
 
   <h1>Et de l'équipe...</h1>
 
+</main>
 </template>
