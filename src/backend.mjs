a45fb3b0d0e7c88c0a22c1baaa0e8f64cfbbbbaf
YannPernette <yann.pernette@edu.univ-fcomte.fr>
2023-05-22 09:55:46 +0200

    fonction appel records backend

--- src/backend.mjs
+++ src/backend.mjs
@@ -1,3 +1,7 @@
 import PocketBase from 'pocketbase';
 const pb = new PocketBase('http://127.0.0.1:8090');
 
+export async function allArticles() {
+    const records = await pb.collection('articles').getFullList();
+    return records;
+}
