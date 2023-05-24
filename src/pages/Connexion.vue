45a29c4f181c3498b6e46931d9753e8e0ec04351
steak-osaure <marion.mura090@gmail.com>
2023-05-24 15:59:07 +0200

    correction margin de trop dans main

--- src/pages/Connexion.vue
+++ src/pages/Connexion.vue
@@ -11,7 +11,7 @@
 
 
 <template>
-  <main class="pt-20 bg-grisbg text-white">
+  <main class="bg-grisbg text-white">
     <h1 class="ml-[2rem] mr-[2rem] p-4 text-center text-violet border-2 rounded-lg">Connexion</h1>
     
     <form>
