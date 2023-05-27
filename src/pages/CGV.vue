78c5bc04861959b909a7de7e8b80b47122efdea0
Rose-Aline <116867946+Rose-Aline@users.noreply.github.com>
2023-05-28 01:11:06 +0200

    page SVG responsive pc et telephone

---
+++ src/pages/CGV.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import Fleche from '../components/icons/Flechehaut.vue';
+</script>
+
+<template>
+
+    <main class="bg-grisbg text-white">
+   
+        <div class="space-y-2 bg-noirbackground lg:bg-transparent">
+            <h1 class="text-violet pt-10 text-center lg:text-white lg:font-cabin lg:text-left lg:px-12 font-medium">Conditions générales de vente</h1>
+            <hr class="text-violet lg:relative lg:border-t-2 lg:hidden">
+        </div>
+        <div class=" pb-20 px-12 ">
+            <p class="pt-6">L&apos;abonnement premium disponible sur IAmateur est à 4 euros/mois, le prix ne dépassera jamais ce montant et restera fixe aucune réduction ne peut être faite.</p>
+            <p class="pt-6">Sur le site le règlement est uniquement possible par carte de crédit ou encore un compte paypal. Il n&apos;est pas possible d&apos; être remboursé après avoir effectué le paiement. Le paiement doit s&apos;effectuer chaque mois, il n&apos;est pas possible de payer à l&apos;avance pour plusieurs mois ou l&apos;année.</p>
+            <p class="pt-6">Il n&apos;est pas possible de disposer de l&apos;abonnement sans payer au préalable, si à la fin du mois le paiement n&apos;a pas été effectué alors l&apos;abonnement prendra fin vous serez redirigé vers votre compte gratuit et vous ne pourrez plus accéder à vos favoris et articles enregistrés.</p>
+            <p class="pt-6">La vente est effectuée envers le service IAmateur, et l&apos;argent sera reçu et départagé entre les 3 gérants d&apos;IAmateur ci-dessous:</p>  
+            <ul class="list-disc list-inside"> 
+                <li>Aline ROSE : <a href="mailto:aline.rose@edu.univ-fcomte.fr">aline.rose@edu.univ-fcomte.fr</a> </li>
+                <li>Marion MURA : <a href="mailto:marion.mura@edu.univ-fcomte.fr">marion.mura@edu.univ-fcomte.fr</a></li>
+                <li>Yann PERNETTE : <a href="mailto:yann.pernette@edu.univ-fcomte.fr">yann.pernette@edu.univ-fcomte.fr</a></li>
+            </ul>
+            <a href="#" class="flex flex-row-reverse"><Fleche /></a>
+        </div>
+    </main>
+</template>
