095553bf17a2255234714586d3e511d86d915e75
Marion MMi 1 <112632202+marionmura@users.noreply.github.com>
2023-05-29 21:38:32 +0200

    Create PageConfidentialite.vue - aline branche recup

---
+++ src/pages/PageConfidentialite.vue
@@ -0,0 +1,94 @@
+<script setup lang="ts">
+import Fleche from '../components/icons/Flechehaut.vue';
+</script>
+
+<template>
+
+    <main class="bg-grisbg text-white">
+   
+        <div class="space-y-2 bg-noirbackground lg:bg-transparent">
+            <h1 class="text-violet pt-10 text-center lg:text-white lg:font-cabin lg:text-left lg:px-12 font-medium text-2xl">Politique de confidentialité</h1>
+            <hr class="text-violet lg:relative lg:border-t-2 lg:hidden">
+        </div>
+        <div class=" pb-20 px-12">
+
+            <h3 class="pt-6 text-lg ">1. informations personnelles collectées</h3>
+
+            <p class="pt-6"> Nous recueillons des informations qui, seuls ou en combinaison avec d&apos;autres informations en notre possession, pourraient être utilisées pour vous identifier comme suit :</p>
+                <p>Nous pouvons recueillir des renseignements personnels si vous créez un compte pour utiliser nos services ou pour communiquer avec nous - comme suit :</p>
+                <p>- Renseignements sur le compte : Lorsque vous créez un compte avec nous, nous recueillons les renseignements associés à votre compte, y compris votre nom, (possiblement votre adresse email) vos coordonnées, vos identifiants de compte, les renseignements sur votre carte de paiement et l&apos;historique des transactions.</p>
+
+            <p class="pt-6">Contenu utilisateur : Lorsque vous utilisez nos Services, nous pouvons recueillir des renseignements personnels qui sont inclus dans les entrées, les téléchargements de fichiers ou les commentaires que vous fournissez envers nos Services.</p>
+                <p>- Renseignements sur la communication : Si vous communiquez avec nous, nous pouvons recueillir votre nom, vos coordonnées et le contenu des messages que vous avez envoyés.</p>
+                <p>- Informations sur les médias sociaux : Nous avons des pages sur des sites de médias sociaux comme Instagram, Facebook, Tiktok, Twitter et LinkedIn. Lorsque vous interagissez avec nos pages de médias sociaux, nous recueillons les renseignements personnels que vous choisissez de nous fournir, comme vos coordonnées. En outre, les entreprises qui hébergent nos pages de médias sociaux peuvent nous fournir des informations et des analyses sur notre activité de médias sociaux.</p>
+                <p>- Renseignements personnels que nous recevons automatiquement de votre utilisation des Services : Lorsque vous visitez, utilisez et interagissez avec les Services:</p>
+
+            <p class="pt-6">Données de connexion : Informations que votre navigateur envoie automatiquement chaque fois que vous utilisez notre site Web. Les données du journal comprennent votre adresse de protocole Internet, le type de navigateur et les paramètres, la date et l&apos;heure de votre demande, et la façon dont vous avez interagi avec notre site Web.</p>
+            <p class="pt-6">Données d&apos;utilisation : Nous pouvons recueillir automatiquement des informations sur votre utilisation des Services, telles que les types de contenu que vous consultez ou utilisez, les fonctionnalités que vous utilisez et les actions que vous effectuez, ainsi que votre fuseau horaire, pays, dates et heures d&apos;accès, agent utilisateur et version, type d&apos;ordinateur ou d&apos;appareil mobile, connexion à l&apos;ordinateur, adresse IP, etc.</p>  
+            <p class="pt-6">Informations sur l&apos;appareil : comprend le nom de l&apos;appareil, le système d&apos;exploitation et le navigateur que vous utilisez. Les informations recueillies peuvent dépendre du type d&apos;appareil que vous utilisez et de ses paramètres.</p>
+            <p class="pt-6">Cookies : Nous utilisons des cookies pour exploiter et administrer nos Services, et améliorer votre expérience à ce sujet. Un « témoin » est une information envoyée à votre navigateur par un site Web que vous visitez. Vous pouvez configurer votre navigateur pour accepter tous les cookies, pour rejeter tous les cookies, ou pour vous avertir chaque fois qu&apos;un cookie est proposé afin que vous puissiez décider à chaque fois de l&apos;accepter. Toutefois, le refus d&apos;un cookie peut dans certains cas vous empêcher d&apos;utiliser, ou affecter négativement l&apos;affichage ou la fonction d&apos;un site Web ou de certaines zones ou fonctionnalités d&apos;un site Web. Pour plus de détails sur les cookies, visitez Tout sur les cookies. </p>
+            <p class="pt-6">Analyse : Nous pouvons utiliser divers produits d&apos;analyse en ligne qui utilisent des témoins pour nous aider à analyser la façon dont les utilisateurs utilisent nos Services et à améliorer votre expérience lorsque vous utilisez les Services. Commentaires : Quand vous laissez un commentaire sur notre site web, les données inscrites dans le formulaire de commentaire, mais aussi votre adresse IP et l&apos;agent utilisateur de votre navigateur sont collectés pour nous aider à la détection des commentaires indésirables. </p>
+
+            <h3 class="pt-6 text-lg ">2. Comment nous utilisons les informations personnelles</h3>
+            <p class="pt-6">Nous pouvons utiliser les renseignements personnels aux fins suivantes :</p>
+            <ul class="list-disc list-inside"> 
+                <li>fournir, administrer, maintenir, améliorer et/ou analyser les Services ;</li>
+                <li>mener des recherches ;</li>
+                <li>pour communiquer avec vous ;</li>
+                <li>élaborer de nouveaux programmes et services ;</li>
+            </ul>
+
+            <p class="pt-6">Prévenir la fraude, les activités criminelles, l&apos;utilisation abusive de nos services et assurer la sécurité de nos systèmes, de notre architecture et de nos réseaux ; Respecter les obligations légales et les procédures légales et protéger nos droits, notre vie privée, notre sécurité ou nos biens, et/ou ceux de nos affiliés, vous ou d&apos;autres tiers.</p>
+            <p class="pt-6">De plus, de temps à autre, nous pouvons analyser le comportement général et les caractéristiques des utilisateurs de nos Services et partager des informations agrégées comme des statistiques générales d&apos;utilisateurs avec des tiers, publier ces informations agrégées ou les rendre généralement disponibles. Nous pouvons collecter des informations agrégées par le biais des Services, des cookies et d’autres moyens décrits dans la présente Politique de confidentialité. Nous conserverons et utiliserons les renseignements dépersonnalisés sous une forme anonyme ou dépersonnalisée et nous n&apos;essaierons pas de les réidentifier.</p>
+
+            <h3 class="pt-6 text-lg ">3. Vos droits</h3>
+            <p class="pt-6">Vous pouvez avoir le droit :</p>
+            <ul class="list-disc list-inside"> 
+                <li>d&apos;accéder à vos renseignements personnels.</li>
+                <li>de supprimer vos renseignements personnels.</li>
+                <li>de corriger ou mettre à jour vos renseignements personnels.</li>
+                <li>de retirer votre consentement au traitement de vos renseignements personnels.</li>
+            </ul>
+            <p>Vous pouvez exercer certains de ces droits via notre formulaire de contact.</p>
+
+            <h3 class="pt-6 text-lg ">4. Enfants</h3>
+            <p class="pt-6">Notre Service ne s&apos;adresse pas aux enfants de moins de 13 ans. IAmateur ne recueille pas de renseignements personnels auprès d&apos;enfants de moins de 13 ans. Si vous avez des raisons de croire qu&apos;un enfant de moins de 13 ans a fourni des renseignements personnels à IAmateur, veuillez nous envoyer un courriel par notre formulaire de contact. Nous enquêterons sur toute notification et, dans le cas échéant, supprimerons les renseignements personnels de nos systèmes.</p>
+
+            <h3 class="pt-6 text-lg ">5. Liens vers d&apos;autres sites Web</h3>
+            <p class="pt-6">Le Service peut contenir des liens vers d&apos;autres sites Web non exploités ou contrôlés par IAmateur, y compris les services de médias sociaux (« sites tiers »). Les informations que vous partagez avec des Sites tiers seront régies par les politiques de confidentialité et les conditions de service spécifiques des Sites tiers et non par la présente Politique de confidentialité. En fournissant ces liens, nous ne sous-entendons pas que nous approuvons ou avons examiné ces sites. Veuillez communiquer directement avec les sites tiers pour obtenir des renseignements sur leurs pratiques et politiques en matière de protection des renseignements personnels.</p>
+
+            <h3 class="pt-6 text-lg ">6. Communication de renseignements personnels</h3>
+            <p class="pt-6">Dans certaines circonstances, nous pouvons fournir vos renseignements personnels à des tiers sans préavis, sauf si la loi l&apos;exige :</p>
+            <p class="pt-6">Fournisseurs et fournisseurs de services : Pour nous aider à répondre aux besoins opérationnels et à exécuter certains services et fonctions, nous pouvons fournir des renseignements personnels aux fournisseurs de services, y compris les fournisseurs de services d&apos;hébergement, les logiciels de communication par courriel ainsi que les services d&apos;analyse Web. Conformément à nos instructions, ces parties accéderont, traiteront ou stockent des renseignements personnels uniquement dans l&apos;exercice de leurs fonctions.</p>
+            <p class="pt-6">Sociétés affiliées : Nous pouvons divulguer des renseignements personnels à nos sociétés affiliées, c&apos;est-à-dire une entité qui contrôle, est contrôlée par, ou est sous contrôle commun avec IAmateur. Nos affiliés peuvent utiliser les Informations personnelles que nous partageons d&apos;une manière compatible avec la présente Politique de confidentialité.</p>
+
+            <h3 class="pt-6 text-lg ">7. Exigences d&apos;utilisation</h3>
+            <p class="pt-6">Utilisation des Services. Vous pouvez accéder aux Services, et nous vous accordons un droit non exclusif d&apos;utilisation, conformément aux présentes Conditions. Vous respecterez ces Conditions et toutes les lois applicables lors de l&apos;utilisation des Services. Nous et nos affiliés possédons tous les droits, titres et intérêts sur les Services.</p>
+            <p class="pt-6">Commentaires. Nous apprécions les commentaires, les idées, les propositions et les suggestions d&apos;amélioration. Si vous fournissez l&apos;une de ces choses, nous pouvons l&apos;utiliser sans restriction ou compensation pour vous.</p>
+            <p class="pt-6">Restrictions. Vous ne pouvez utiliser les Services d&apos;une manière qui viole, détourne ou viole les droits d&apos;une personne ; utiliser les résultats des Services pour développer des sites qui entrent en concurrence avec IAmateur; utiliser toute méthode automatisée ou programmatique pour extraire des données ou des extrants des Services, y compris le raclage, la collecte de données Web ou l&apos;extraction de données Web; acheter, vendre ou transférer des informations sans notre consentement préalable.</p>
+            <p class="pt-6">Services de tiers. Tous les logiciels, services ou autres produits tiers que vous utilisez dans le cadre des Services sont soumis à leurs propres conditions, et nous ne sommes pas responsables des produits tiers.</p>
+
+            <h3 class="pt-6 text-lg ">8. Frais et paiements à l'abonnement premium</h3>
+            <p class="pt-6">L&apos;abonnement peut être annulé à tout moment, tout paiement déjà envoyé à IAmateur ne sera pas remboursé.</p>
+            <p class="pt-6">Sauf indication contraire, les frais ne comprennent pas les taxes, droits et autres cotisations fédérales, étatiques, locales et étrangères. Vous êtes responsable de toutes les taxes associées à votre achat, à l&apos;exclusion des taxes basées sur notre revenu net, et nous pouvons vous facturer ces taxes. Vous acceptez de payer ces taxes en temps opportun et de nous fournir des documents indiquant le paiement, ou des preuves supplémentaires que nous pouvons raisonnablement exiger. Nous pouvons modifier nos prix en publiant un avis sur votre compte et/ou sur notre site Web. Les augmentations de prix entreront en vigueur 14 jours après leur publication, sauf pour les augmentations effectuées pour des raisons juridiques, qui seront en vigueur immédiatement. Toute modification de prix s&apos;appliquera aux frais portés à votre compte immédiatement après la date d&apos;entrée en vigueur des modifications.</p>
+            <p class="pt-6">Si vous souhaitez contester des frais ou des taxes, veuillez communiquer par courriel dans les trente (30) jours suivant la date de la facture contestée.</p>
+
+            <h3 class="pt-6 text-lg ">9. Indemnisation, renonciation aux garanties, limitations de responsabilité</h3>
+            <p class="pt-6">Vous nous défendez, nous indemniserez et nous dégagez de toute responsabilité, ainsi que nos sociétés affiliées et notre personnel, à l&apos;égard de toute réclamation, perte et dépense (y compris les honoraires d&apos;avocat) découlant de votre utilisation des Services, y compris de votre Contenu, ou se rapportant à celle-ci, les produits ou services que vous développez ou offrez en relation avec les Services, et votre violation des présentes Conditions ou de la loi applicable.</p>
+            <p class="pt-6">Les services sont fournis « tels quels ». sauf dans la mesure interdite par la loi, nous, nos sociétés affiliées et nos concédants ne donnons aucune garantie (expresse, implicite, légale ou autre) concernant les services, et déclinons toute garantie, y compris mais sans s&apos;y limiter aux garanties de qualité marchande, aptitude à un usage particulier, qualité satisfaisante, absence de contrefaçon, tranquillité et garanties découlant de toute activité commerciale. Nous ne garantissons pas que les services seront ininterrompus, exacts ou exempts d&apos;erreurs, ou que tout contenu sera sécurisé ou ne sera ni perdu ni modifié. Ni nous, ni nos sociétés affiliées ni nos concédants de licence ne serons responsables de tout dommage indirect, accessoire, spécial, consécutif ou exemplaire, y compris les dommages pour perte de profits, de bonne utilisation, de données ou autres pertes, même si nous avons été informés de la possibilité de tels dommages. Notre responsabilité totale en vertu des présentes modalités ne doit pas dépasser le plus élevé du montant que vous avez payé pour le service qui a donné lieu à la réclamation au cours des 12 mois précédant la survenance de la responsabilité ou de cent dollars (100 $). Les limitations de cette section ne s&apos;appliquent que dans la mesure maximale permise par la loi applicable.</p> 
+
+            <h3 class="pt-6 text-lg ">10. Modifications apportées à la politique de confidentialité</h3>
+            <p class="pt-6">Nous pouvons modifier cette Politique de confidentialité à tout moment. Lorsque nous le ferons, nous publierons une version mise à jour sur cette page, sauf si un autre type d&apos;avis est requis par la loi applicable. En continuant à utiliser notre Service ou en nous fournissant des Informations personnelles après que nous ayons publié une Politique de confidentialité mise à jour, ou vous en avoir informé par d&apos;autres moyens, vous consentez à la Politique de confidentialité révisée.</p>
+
+            <h3 class="pt-6 text-xl">Si vous avez besoin d&apos;aide, contactez :</h3>
+
+            <ul> 
+                <li class="lg:pt-3">Aline ROSE : <a href="mailto:aline.rose@edu.univ-fcomte.fr">aline.rose@edu.univ-fcomte.fr</a> </li>
+                <li class="lg:pt-3">Marion MURA : <a href="mailto:marion.mura@edu.univ-fcomte.fr">marion.mura@edu.univ-fcomte.fr</a></li>
+                <li class="lg:pt-3">Yann PERNETTE : <a href="mailto:yann.pernette@edu.univ-fcomte.fr">yann.pernette@edu.univ-fcomte.fr</a></li>
+            </ul>
+
+            <a href="#" class="flex flex-row-reverse"><Fleche /></a>
+        </div>
+    </main>
+</template>
