2aaa5765f81b79f8c80495264d857917ca180054
steak-osaure <marion.mura090@gmail.com>
2023-05-29 21:21:42 +0200

    correction margin de trop pour le header lg:hidden

--- src/components/HeaderPage.vue
+++ src/components/HeaderPage.vue
@@ -9,10 +9,10 @@ import {RouterLink} from 'vue-router'
 
 <template >
     <header class="p-4 bg-noirbackground text-violet h-20 relative fixed z-10 w-full">
-        <div class="flex justify-between items-center px-2 lg:grid lg:grid-cols-2">
+        <div class="flex justify-between items-center px-2 lg:grid lg:grid-cols-2 ">
             <RouterLink class="" to="/"><IAmateurlogo/></RouterLink>
 
-            <div class="flex  items-center gap-4 lg:hidden">
+            <div class="flex  items-center gap-4 lg:hidden ">
                 <RouterLink to="/Connexion"><Usericon/></RouterLink>
 
                 <button class="relative z-10 h-4 w-6 flex flex-col justify-between lg:hidden"
@@ -23,7 +23,7 @@ import {RouterLink} from 'vue-router'
                 </button>
             </div>
 
-            <div class="invisible h-[0] w-[0] lg:visible lg:h-[20px] lg:w-[10px] lg:flex lg:flex-row-reverse   lg:justify-end items-center">
+            <div class="hidden h-[0] w-[0]  lg:visible lg:h-[20px] lg:w-[10px] lg:flex lg:flex-row  items-center ">
                 <RouterLink to="/Connexion">
                     <button class="flex-1 m-2 p-2 bg-white rounded-lg text-noirbackground font-bold ">Connexion</button>
                 </RouterLink>
@@ -34,7 +34,7 @@ import {RouterLink} from 'vue-router'
 
             </div>
         </div>
-        <nav class="invisible opacity-0  lg:invisible lg:relative fixed inset-0 h-screen w-screen bg-noirbackground"  :class="{'!visible !opacity-100':menuIsOpen}">
+        <nav class="invisible opacity-0  lg:hidden lg:relative fixed inset-0 h-screen w-screen bg-noirbackground"  :class="{'!visible !opacity-100':menuIsOpen}">
             <ul class="mt-[80px] ml-[30px] space-x-10 flex flex-col gap-4 ">
                 <li><RouterLink class="active" to="/Espacepersonnel" >Espace personnel</RouterLink></li>
                 <li><RouterLink class="menu-link" to="/Mesfavoris">Mes favoris</RouterLink></li>
