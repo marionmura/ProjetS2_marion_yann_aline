9ca990519353240d5b8cde24d5d89352fa5ce3aa
Marion MMi 1 <112632202+marionmura@users.noreply.github.com>
2023-05-29 21:39:05 +0200

    Create PolitiqueCookies.vue - alinebranche recup

---
+++ src/pages/PolitiqueCookies.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import Fleche from '../components/icons/Flechehaut.vue';
+</script>
+
+<template>
+
+    <main class="bg-grisbg text-white">
+   
+        <div class="space-y-2 bg-noirbackground lg:bg-transparent">
+            <h1 class="text-violet pt-10 text-center lg:text-white lg:font-cabin lg:text-left lg:px-12 font-medium text-2xl">Politique des cookies</h1>
+            <hr class="text-violet lg:relative lg:border-t-2 lg:hidden">
+        </div>
+        <div class=" pb-20 px-12">
+
+            <h3 class="pt-6 text-lg ">Pourquoi nous utilisons des cookies ?</h3>
+
+            <p> Les cookies sont nécessaires au bon fonctionnement du site comme il vous l&apos;a été vendu et conçu. Les cookies nous permettent de reconnaître le type d&apos;utilisateur et son abonnement éventuel pour lui accorder les services qui lui sont dus.</p>
+            <p class="pt-6">Nous utilisons des cookies pour analyser comment les Services sont accédés, utilisés et leurs performances. Nous utilisons ces informations pour maintenir et continuer d&apos;améliorer le bon fonctionnement du site IAmateur. Nous pouvons également détenir vos informations comme votre nom, vos coordonnées, votre adresse mail, vos moyens de paiement et l&apos;historique de vos transactions dont vous pouvez demander l&apos;effacement dans la politique de confidentialité.</p>
+            <p class="pt-6">Ces cookies nous permettent d&apos;opérer au bon fonctionnement du site selon vos préférences, lorsque vous revenez sur le site web IAmateur nous pouvons vous fournir les options disponibles en fonction de votre abonnement et vos réglages en fonction des données que vous nous avez fournies.</p>
+            <p class="pt-6">Nous utilisons également ces cookies pour personnaliser les publicités qui sont sur le site lorsque vous avez l&apos;offre gratuite. Les publicités sont choisies en fonction de vos centres d&apos;intérêts et ces informations peuvent être utilisées pour ré-afficher plusieurs fois la même publicité lorsque vous vous connectez au site web pour mesurer leur efficacité.</p>  
+            <p class="pt-6">Nous travaillons en collaboration avec d&apos;autres sites web qui souhaiteraient faire la promotion de leur site web concernant les IA sur IAmateur et les cookies recueillis peuvent également leur servir à mesurer l&apos;efficacité de leur promotion.</p>
+
+            <h3 class="pt-6 text-lg">Comment changer la préférence des cookies sur votre navigateur ?</h3>
+
+            <p>Vous pouvez à tout moment changer vos préférences si vous souhaitez interdire l&apos;utilisation des cookies par votre navigateur dans les paramètres > confidentialité et sécurité > cookies et autres données des sites et choisir de supprimer les cookies sur le site IAmateur.</p>  
+            <p class="pt-6">Mais notez bien que si vous supprimez les cookies cela peut altérer le bon fonctionnement du site et vous ne serez peut être pas en mesure de voir tout le contenu affiché sur IAmateur. </p>
+                
+            <h3 class="pt-6 text-lg">Mises à jour de la politique des cookies.</h3>
+            <p> Nous mettrons régulièrement à jour la politique des cookies comme la politique de confidentialité, vous serez notifié par email lors du changement de celles-ci. En continuant à utiliser notre Service ou en nous fournissant des Informations personnelles après que nous ayons publié une Politique de cookies mise à jour, ou vous en avoir informé par d&apos;autres moyens, vous consentez à la Politique de confidentialité révisée.</p>  
+
+            <h3 class="pt-6 text-xl">Si vous avez besoin d&apos;aide, contactez :</h3>
+
+            <ul> 
+                <li class="lg:pt-3">Aline ROSE : <a href="mailto:aline.rose@edu.univ-fcomte.fr">aline.rose@edu.univ-fcomte.fr</a> </li>
+                <li class="lg:pt-3">Marion MURA : <a href="mailto:marion.mura@edu.univ-fcomte.fr">marion.mura@edu.univ-fcomte.fr</a></li>
+                <li class="lg:pt-3">Yann PERNETTE : <a href="mailto:yann.pernette@edu.univ-fcomte.fr">yann.pernette@edu.univ-fcomte.fr</a></li>
+            </ul>
+
+            <a href="#" class="flex flex-row-reverse"><Fleche /></a>
+        </div>
+    </main>
+</template>
