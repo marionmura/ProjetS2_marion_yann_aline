636ff71c8cfc89096b7acb6f5596c945dea1c1e1
steak-osaure <marion.mura090@gmail.com>
2023-06-03 13:00:29 +0200

    test correction

--- src/backend.ts
+++ src/backend.ts
@@ -1,8 +1,9 @@
-import PocketBase from 'PocketBase';
+import pocketbase from 'pocketbase';
+export const pb = new pocketbase('http://127.0.0.1:8090');
+import { ArticlesResponse } from './pocketbase-types';
 
-export const pb = new PocketBase('http://127.0.0.1:8090');
 
 export async function allArticles(){
-    const records = await pb.collection('articles').getFullList();
+    const records = await pb.collection('articles').getFullList<ArticlesResponse>({'$autoCancel': false,});
     return records;
 }
